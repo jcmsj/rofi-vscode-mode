b2fbe5ae73b6de68f446410d76ffd39f2506d028
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2024-05-30 14:03:59 +0800

    WIP: nix

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, fenix, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages.default =
+        let
+          toolchain = fenix.packages.${system}.minimal.toolchain;
+          pkgs = nixpkgs.legacyPackages.${system};
+          lib = pkgs.lib;
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = toolchain;
+            rustc = toolchain;
+          };
+        in
+        rustPlatform.buildRustPackage rec {
+          pname = "rofi-vscode-mode";
+          version = "0.6.2";
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+          buildInputs = with pkgs; [
+            glib
+            cairo
+            pango
+            sqlite
+          ];
+          src = ./.;
+
+          cargoLock.lockFile = ./Cargo.lock;
+          meta = {
+            description = "A Rofi mode to open Visual Studio Code workspaces";
+            homepage = "https://github.com/fuljo/rofi-vscode-mode";
+            license = lib.licenses.mit;
+            maintainers = [ ];
+          };
+        };
+    });
+}
